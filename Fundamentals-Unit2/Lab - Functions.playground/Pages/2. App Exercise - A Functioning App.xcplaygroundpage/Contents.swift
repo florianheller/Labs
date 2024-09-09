c991d86862f0714fe64e4d3e822f93fc93881454
Florian Heller <florian@heller-web.net>
2024-09-10 01:12:12 +0200

    Functions

--- Fundamentals-Unit2/Lab - Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
+++ Fundamentals-Unit2/Lab - Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
@@ -8,10 +8,40 @@
  A reoccurring process like this is a perfect candidate for a function. Write a function called `incrementSteps` after the declaration of `steps` below that will increment `steps` by one and then print its value. Call the function multiple times and observe the printouts.
  */
 var steps = 0
-
+func incrementSteps() {
+    steps += 1
+    print(steps)
+}
+incrementSteps()
+incrementSteps()
+incrementSteps()
 //:  Similarly, if you want to regularly provide progress updates to your user, you can put your control flow statements that check on progress into a function. Write a function called `progressUpdate` after the declaration of `goal` below. The function should print "You're off to a good start." if `steps` is less than 10% of `goal`, "You're almost halfway there!" if `steps` is less than half of `goal`, "You're over halfway there!" if `steps` is less than 90% of `goal`, "You're almost there!" if `steps` is less than `goal`, and "You beat your goal!" otherwise. Call the function and observe the printout.
 let goal = 10000 
+func progressUpdate(steps: Int) {
+    let steps = Double(steps)
+    
+    if steps < Double(goal) / 10 {
+        print("You're off to a good start.")
+    }
+    else if steps < Double(goal) / 2 {
+        print("You're almost halfway there!")
+    }
+    else if steps < Double(goal) * 0.9 {
+        print("You're over halfway there!")
+    }
+    else if steps < Double(goal) {
+        print("You're almost there!")
+    }
+    else {
+        print("You beat your goal!")
+    }
+}
 
+progressUpdate(steps: 100)
+progressUpdate(steps: 3500)
+progressUpdate(steps: 5050)
+progressUpdate(steps: 9902)
+progressUpdate(steps: 10500)
 
 /*:
 [Previous](@previous)  |  page 2 of 6  |  [Next: Exercise - Parameters and Argument Labels](@next)
--- Fundamentals-Unit2/Lab - Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
+++ Fundamentals-Unit2/Lab - Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
@@ -9,10 +9,39 @@
  
  Call the function a number of times, passing in different values of `steps` and `goal`. Observe the printouts and make sure what is printed to the console is what you would expect for the parameters passsed in.
  */
+func progressUpdate(steps: Int, goal: Int) {
+    let steps = Double(steps)
+    
+    if steps < Double(goal) / 10 {
+        print("You're off to a good start.")
+    }
+    else if steps < Double(goal) / 2 {
+        print("You're almost halfway there!")
+    }
+    else if steps < Double(goal) * 0.9 {
+        print("You're over halfway there!")
+    }
+    else if steps < Double(goal) {
+        print("You're almost there!")
+    }
+    else {
+        print("You beat your goal!")
+    }
+}
 
-
+progressUpdate(steps: 1500, goal: 10000)
+progressUpdate(steps: 1501, goal: 3000)
 //:  Your fitness tracking app is going to help runners stay on pace to reach their goals. Write a function called pacing that takes four `Double` parameters called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. Your function should calculate whether or not the user is on pace to hit or beat `goalTime`. If yes, print "Keep it up!", otherwise print "You've got to push it just a bit harder!"
-
+// Sorry, I don't get the calculation here...
+func pacing(currentDistance: Double, totalDistance: Double, currentTime: Double, goalTime: Double) {
+    // Calculate the current speed and compare it to the goal speed
+    if (currentDistance / currentTime) < (totalDistance / goalTime) {
+        print("You've got to push it just a bit harder!")
+    }
+    else {
+        print("Keep it up!")
+    }
+}
 
 /*:
 [Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Return Values](@next)
